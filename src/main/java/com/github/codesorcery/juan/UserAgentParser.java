15cd837936d3ef25c1c04ac5b7d995164895888f
Patrick Marx <mageofcode@gmail.com>
2018-12-31 11:31:07 +0100

    Add option to log tokenized UA via custom logging function

--- src/main/java/com/github/codesorcery/juan/UserAgentParser.java
+++ src/main/java/com/github/codesorcery/juan/UserAgentParser.java
@@ -8,9 +8,12 @@ import com.github.codesorcery.juan.token.TokenizedUserAgent;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class UserAgentParser {
     private final CombinedDeviceLookup deviceLookup;
+    private Consumer<Supplier<String>> logger;
 
     private UserAgentParser(final DeviceLookup... deviceLookups) {
         deviceLookup = new CombinedDeviceLookup(deviceLookups);
@@ -36,9 +39,17 @@ public class UserAgentParser {
         );
     }
 
+    public UserAgentParser withTokenizedUALogger(final Consumer<Supplier<String>> loggingFunction) {
+        this.logger = loggingFunction;
+        return this;
+    }
+
     public ParsedUserAgent parse(final String userAgentString) {
         final TokenizedUserAgent tokenizedUserAgent = TokenizedUserAgent
                 .forUserAgentString(userAgentString);
+        if (logger != null) {
+            logger.accept(tokenizedUserAgent::toString);
+        }
         final Agent agent = Agent.fromUserAgent(tokenizedUserAgent);
         final OperatingSystem os = OperatingSystem.fromUserAgent(tokenizedUserAgent);
         final DeviceInfo device = deviceLookup.getDeviceInfo(tokenizedUserAgent)
--- src/test/java/com/github/codesorcery/juan/UserAgentParserTest.java
+++ src/test/java/com/github/codesorcery/juan/UserAgentParserTest.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserAgentParserTest {
+    private static final Logger LOG = Logger.getLogger(UserAgentParser.class.getSimpleName()) ;
+
     @Test
     public void withoutDeviceList() {
-        final UserAgentParser userAgentParser = UserAgentParser.withoutPlayStoreDeviceList();
+        final UserAgentParser userAgentParser = UserAgentParser
+                .withoutPlayStoreDeviceList()
+                .withTokenizedUALogger(LOG::info);
 
         final String input = "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36";
         final ParsedUserAgent result = userAgentParser.parse(input);
@@ -36,8 +41,12 @@ public class UserAgentParserTest {
     @Test
     public void withDeviceList() throws IOException {
         final ClassLoader classLoader = getClass().getClassLoader();
-        final UserAgentParser userAgentParser = UserAgentParser.withPlayStoreDeviceList(
-                classLoader.getResource("supported_devices_subset.csv"), Charset.forName("UTF-8"));
+        final UserAgentParser userAgentParser = UserAgentParser
+                .withPlayStoreDeviceList(
+                        classLoader.getResource("supported_devices_subset.csv"),
+                        Charset.forName("UTF-8")
+                )
+                .withTokenizedUALogger(LOG::info);
 
         final String input = "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36";
         final ParsedUserAgent result = userAgentParser.parse(input);
